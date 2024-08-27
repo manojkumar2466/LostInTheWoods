036e2a384aceea023aac4ebc62f74d55751805bd
Manoj Kumar Mutukuloju <manojcareer18@gmail.com>
2024-08-27 15:48:30 -0400

    Part8 - Player holding weapon and related animation created

---
+++ Source/LostInTheWoods/Private/Interfaces/Pickup.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interfaces/Pickup.h"
+
+// Add default functionality here for any IPickup functions that are not pure virtual.
+
+
---
+++ Source/LostInTheWoods/Public/EnumTypes.h
@@ -0,0 +1,16 @@
+#pragma once
+
+
+UENUM(BlueprintType)
+enum class ECharacterWeaponEquipState : uint8
+{
+	ECWES_UnEquipped UMETA(DisplayName= "NoWeapon"),
+	ECWES_Equipped UMETA(DisplayName="HaveWeapon")
+};
+
+UENUM(BlueprintType)
+enum class ECharacterActionState : uint8
+{
+	ECAS_Unoccupied UMETA(DisplayName="Unoccupied"),
+	ECAS_Attacking UMETA(DisplayName="Attacking")
+};
