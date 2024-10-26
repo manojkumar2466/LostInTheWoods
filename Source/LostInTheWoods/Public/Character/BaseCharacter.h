a8dc408d832ddc97d17575fe63ba017dc2834e2c
Manoj Kumar Mutukuloju <manojcareer18@gmail.com>
2024-10-25 20:29:03 -0400

    warrior UI created

--- Source/LostInTheWoods/Public/Character/BaseCharacter.h
+++ Source/LostInTheWoods/Public/Character/BaseCharacter.h
@@ -23,7 +23,7 @@ public:
 
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 	virtual float TakeDamage(float DamageAmount, struct FDamageEvent const& DamageEvent, class AController* EventInstigator, AActor* DamageCauser);
-
+ 
 
 private:
 
